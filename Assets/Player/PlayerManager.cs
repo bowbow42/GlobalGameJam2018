9df7093007d5620740911ceeaf3945fb8f2eeb39
Philipp Helsper <phelsper@gmail.com>
2018-01-26 22:17:05 +0100

    initial commit

---
+++ Assets/Player/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    public GameObject lastSpawn;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+            transform.position = lastSpawn.transform.position;
+    }
+}
---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public int movementSpeed;
+    public int jumpForce;
+
+    private bool inAir = false;
+
+    private Rigidbody2D _rb;
+    private Collider2D _coll;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _coll = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        float leftRightMovement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(leftRightMovement * movementSpeed * Time.deltaTime, 0f, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !inAir)
+            jump();
+    }
+
+    void jump()
+    {
+        _rb.AddForce(transform.up * jumpForce);
+        inAir = true;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+            inAir = false;
+    }
+    
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+            inAir = true;
+    }
+
+}
