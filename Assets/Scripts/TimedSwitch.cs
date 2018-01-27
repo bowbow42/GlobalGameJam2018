ca8565bc4cda4eafc663a3587dd4acc2e5b06255
jellyfishhunter <jellyfishhunter@gmail.com>
2018-01-27 23:30:06 +0100

    TimedSwitch.cs and clockwise GearBehaviour

---
+++ Assets/Scripts/TimedSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedSwitch : SwitchBehaviour
+{
+    public float interval = 2f;
+    private float timeUntilNextAction;
+
+    private void Start()
+    {
+        timeUntilNextAction = interval;
+    }
+
+    private void Update()
+    {
+        timeUntilNextAction -= Time.deltaTime;
+        if (timeUntilNextAction < 0)
+        {
+            timeUntilNextAction = interval;
+            Target.GetComponent<GearBehaviour>().Action();
+        }
+    }
+
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+}
