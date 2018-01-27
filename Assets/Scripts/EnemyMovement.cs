63e3381e14b08cf81a8e981a98e9e7a00dae8eae
jellyfishhunter <jellyfishhunter@gmail.com>
2018-01-27 14:17:34 +0100

    Main Camera.prefab

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public bool goRight;
+	
+	void Update () {
+        if(!goRight)
+            transform.position += new Vector3(-5 * Time.deltaTime, 0, 0);
+        else
+            transform.position += new Vector3(5 * Time.deltaTime, 0, 0);
+
+    }
+}
