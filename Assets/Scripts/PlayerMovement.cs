d4ce6e24daf50668ad2b23d0ee9843ac914df4c4
fleet3123 <lorenz.wech@web.de>
2018-01-27 19:27:36 +0100

    multijump, multidash and sliding

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,22 @@ public class PlayerMovement : MonoBehaviour
     public float movementDampening;
     public float airSpeed;
     public float jumpForce;
+    public float dashForce;
+    public float dashTimeOut;
+    public int dashCounterMax = 3;
     public float leaveGroundInterval;
     public float leaveGroundDistance;
     public Vector2 Gravity;
     public Vector2 MaxVelocity;
 
     private bool inAir = true;
+    private bool multiJump = false;
+    public int multiJumpCounterMax = 1;
+    private int jumpCounter = 0;
+
+    private float dashStart;
+    private int dashCounter = 0; 
+    private bool dashing = false;
 
     private Vector2 FloorNormal;
     private Vector2 MovementDir;
@@ -22,6 +32,11 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 Acceleration = new Vector2();
 
     private float leaveGroundStart;
+
+    private int beginSlideCounter = 3;
+    private int slideCounter = 0;
+    private bool sliding = false;
+
     //private Rigidbody2D _rb;
     private Collider2D _coll;
     private CircleCollider2D _circle;
@@ -42,7 +57,7 @@ public class PlayerMovement : MonoBehaviour
 
 		if (inAir)
 		{
-            Velocity *= 0.95f;
+            //Velocity.y *= 0.99f;
             Acceleration = MovementDir * leftRightMovement * airSpeed + Gravity;
 			Acceleration *= getMobilityFactor();
 
@@ -53,7 +68,10 @@ public class PlayerMovement : MonoBehaviour
         else 
         {
             Velocity *= 0.8f;
-            Acceleration.x = leftRightMovement * movementSpeed;
+            if (sliding)
+                Acceleration = MovementDir * movementSpeed + Gravity;
+            else
+                Acceleration.x = leftRightMovement * movementSpeed;
 			Acceleration *= getMobilityFactor();
 
             Velocity += Acceleration * Time.deltaTime;
@@ -62,7 +80,7 @@ public class PlayerMovement : MonoBehaviour
             Velocity.x = Mathf.Sign(Velocity.x) * Mathf.Min(Mathf.Abs(Velocity.x), MaxVelocity.x);
             Velocity.y = Mathf.Sign(Velocity.y) * Mathf.Min(Mathf.Abs(Velocity.y), MaxVelocity.y);
             transform.position += new Vector3(MovementDir.x * Velocity.x, MovementDir.y * Velocity.x, 0) * Time.deltaTime;
-                        
+            
             if (leaveGroundStart != 0.0f)
             {
                 
@@ -74,10 +92,10 @@ public class PlayerMovement : MonoBehaviour
                 else
                 {
                     RaycastHit2D[] contacts = new RaycastHit2D[1];
-                    Debug.Log("Casting");
+                    //Debug.Log("Casting");
                     if(_coll.Cast(Vector2.down, contacts, leaveGroundDistance, true) > 0)
                     {
-                        Debug.Log(contacts[0].distance);
+                        //Debug.Log(contacts[0].distance);
                         if (contacts[0].collider.CompareTag("Platform"))
                         {
                             if (!(contacts[0].normal.y - 0.5 <= 0))
@@ -90,48 +108,120 @@ public class PlayerMovement : MonoBehaviour
 
             
         }
+        if (dashing)
+        {
+            transform.position += new Vector3(MovementDir.x, MovementDir.y, 0) * dashForce * Mathf.Sign(leftRightMovement);
+            dashing = false;
+            Debug.Log(dashCounter);
+            if(dashCounter > dashCounterMax)
+                dashStart = Time.time;
+        }
 
+        //Debug.Log(Velocity);
         Acceleration = new Vector2();
+
+        if (dashStart > 0.0f && dashStart + dashTimeOut < Time.time)
+        {
+            dashStart = 0.0f;
+            dashCounter = 0;
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !inAir)
-            jump();
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!inAir)
+                jump();
+            else
+            {
+                if (jumpCounter < multiJumpCounterMax)
+                {
+                    ++jumpCounter;
+                    jump();
+                }
+            }
+        }
+        if(Input.GetButtonDown("Dash"))
+        {
+            if(dashStart == 0)
+            {
+                dash();
+                
+            }
+            
+        }
+    }
+
+    void dash()
+    {
+        if (dashCounter <= dashCounterMax)
+        {
+            dashCounter++;
+            dashing = true;
+        }
     }
 
     void jump()
     {
         Velocity.y = jumpForce;
         inAir = true;
+        MovementDir = Vector2.right;
+        slideCounter = 0;
     }
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
         //Debug.Log("Hit something");
-        leaveGroundStart = 0.0f;
+        
         if (collision.gameObject.CompareTag("Platform"))
         {
-            Velocity.y = 0;
+            
+            Vector2 CollisionNormal = new Vector2();
             FloorNormal = new Vector2();
             int count = 0;
             foreach (ContactPoint2D contact in collision.contacts)
             {
+                CollisionNormal += contact.normal;
                 if (contact.normal.y - 0.5 <= 0) continue;
                 FloorNormal += contact.normal;
                 count++;
             }
-            
+
             if (count > 0)
             {
-                MovementDir = new Vector2(FloorNormal.y, -FloorNormal.x );
+                leaveGroundStart = 0.0f;
+                jumpCounter = 0;
+                Velocity.y = 0;
+
+                MovementDir = new Vector2(FloorNormal.y, -FloorNormal.x);
                 if (MovementDir.x < 0)
                     MovementDir *= -1;
-                MovementDir = MovementDir / count;               
+                MovementDir = MovementDir / count;
                 inAir = false;
+                slideCounter = 0;
+                sliding = false;
             }
             else
+            {
+                if ((slideCounter > beginSlideCounter))
+                {
+                    leaveGroundStart = 0.0f;
+                    jumpCounter = 0;
+
+                    sliding = true;
+                    MovementDir = new Vector2(CollisionNormal.y, -CollisionNormal.x);
+                    if (MovementDir.y > 0)
+                        MovementDir *= -1;
+                    inAir = false;
+                    return;
+                }
+                slideCounter++;
                 MovementDir = Vector2.right;
+                Velocity = Vector2.Reflect(Velocity, CollisionNormal.normalized);
+                inAir = true;              
+            }   
         }
     }
 
