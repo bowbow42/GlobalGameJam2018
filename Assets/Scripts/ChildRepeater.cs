a8d1a9c1915735d916021b838cef7c0c7e037ae9
Jan Sobotta <jsobotta@uni-koblenz.de>
2018-01-28 01:04:59 +0100

    added ChildRepeater, fixed errors in scripts

---
+++ Assets/Scripts/ChildRepeater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildRepeater : MonoBehaviour {
+
+    public GameObject ObjectToRepeat;
+    public int ObjectCount;
+    public float Offset;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < ObjectCount; i++ )
+        {
+            GameObject obj = Instantiate(ObjectToRepeat, transform) as GameObject;
+            obj.gameObject.transform.localPosition = new Vector3(0f, 0f, Offset * i);
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
